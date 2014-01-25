5c28aae985beb032013f69c167fcf64d0454eb9c
Katochimoto <bufpost@yandex.ru>
2014-01-26 02:25:53 +0400

    params defined

--- test/spec/tag.param.js
+++ test/spec/tag.param.js
@@ -141,4 +141,165 @@ describe('dox.parseTag() @param', function() {
         expect(tag.optional).to.be.ok();
         expect(tag.description).to.be('text description');
     });
+
+    it('param empty name', function() {
+        var tag = dox.parseTag('@param {String}');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types');
+        expect(tag.type).to.be('param');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+    });
+
+    it('param empty description', function() {
+        var tag = dox.parseTag('@param {String} test');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+    });
+
+    it('optional param empty description', function() {
+        var tag = dox.parseTag('@param {String} [test]');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+    });
+
+    it('optional param empty description by default', function() {
+        var tag = dox.parseTag('@param {String} [test=qwe asd]');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional', 'default');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+        expect(tag.default).to.be('qwe asd');
+    });
+
+    it('optional param not empty description by default', function() {
+        var tag = dox.parseTag('@param {String} [test=qwe asd] text description');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional', 'default', 'description');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+        expect(tag.default).to.be('qwe asd');
+        expect(tag.description).to.be('text description');
+    });
+
+    it('optional param not empty description by default', function() {
+        var tag = dox.parseTag('@param {!String} [test=qwe asd] text description');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional', 'default', 'description', 'nonNullable');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+        expect(tag.nonNullable).to.be.ok();
+        expect(tag.default).to.be('qwe asd');
+        expect(tag.description).to.be('text description');
+    });
+
+    it('optional param not empty description by default', function() {
+        var tag = dox.parseTag('@param {?String} [test=qwe asd] text description');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional', 'default', 'description', 'nullable');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+        expect(tag.nullable).to.be.ok();
+        expect(tag.default).to.be('qwe asd');
+        expect(tag.description).to.be('text description');
+    });
+
+    it('optional param not empty description by default', function() {
+        var tag = dox.parseTag('@param {String=} [test=qwe asd] text description');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional', 'default', 'description');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+        expect(tag.default).to.be('qwe asd');
+        expect(tag.description).to.be('text description');
+    });
+
+    it('optional param not empty description by default', function() {
+        var tag = dox.parseTag('@param {String=} test text description');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name', 'optional', 'description');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ 'String' ]);
+        expect(tag.optional).to.be.ok();
+        expect(tag.description).to.be('text description');
+    });
+
+    it('many types', function() {
+        var tag = dox.parseTag('@param {string|boolean} test');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(2);
+        expect(tag.types).to.eql([ 'string', 'boolean' ]);
+    });
+
+    it('many types', function() {
+        var tag = dox.parseTag('@param {(string|boolean)} test');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(2);
+        expect(tag.types).to.eql([ 'string', 'boolean' ]);
+    });
+
+    it('many types', function() {
+        var tag = dox.parseTag('@param {{a: number, b: string, c}} test');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ '{a: number, b: string, c}' ]);
+    });
+
+    it('many types', function() {
+        var tag = dox.parseTag('@param {*} test');
+        expect(tag).to.be.a(Object);
+        expect(tag).to.only.have.keys('type', 'types', 'name');
+        expect(tag.type).to.be('param');
+        expect(tag.name).to.be('test');
+        expect(tag.types).to.be.a(Array);
+        expect(tag.types).to.have.length(1);
+        expect(tag.types).to.eql([ '*' ]);
+    });
 });
