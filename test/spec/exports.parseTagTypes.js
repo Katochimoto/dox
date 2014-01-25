78f3595795cdde67d62a41392800360b3b91e43f
Katochimoto <bufpost@yandex.ru>
2014-01-25 23:26:53 +0400

    parse tag type test

---
+++ test/spec/exports.parseTagTypes.js
@@ -0,0 +1,10 @@
+if (typeof require == 'function') {
+    /*jshint -W020 */
+    dox = require('../../');
+    /*jshint -W020 */
+    expect = require('expect.js');
+}
+
+describe('exports.parseTagTypes', function() {
+
+});
