e4ef08a78994a29c0cb6fec7dac8767cb9797eef
Katochimoto <bufpost@yandex.ru>
2014-02-05 22:32:15 +0400

    add test parse context

--- test/spec/parse.context.js
+++ test/spec/parse.context.js
@@ -16,4 +16,95 @@ describe('parse.context', function() {
             expect(context).to.eql({});
         });
     });
+
+    it('function statement 1', function() {
+        fixture('function1', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({ type: 'function', name: 'test', string: 'test()' });
+        });
+    });
+
+    it('function statement 2', function() {
+        fixture('function2', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({ type: 'function', name: 'test', string: 'test()' });
+        });
+    });
+
+    it('function expression 1', function() {
+        fixture('function3', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({ type: 'function', name: 'test', string: 'test()' });
+        });
+    });
+
+    it('prototype method 1', function() {
+        fixture('function4', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({
+                type: 'method',
+                name: 'test',
+                string: 'Sample.prototype.test()',
+                cons: 'Sample'
+            });
+        });
+    });
+
+    it('method 1', function() {
+        fixture('function5', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({
+                type: 'method',
+                name: 'test',
+                string: 'proto.test()',
+                receiver: 'proto'
+            });
+        });
+    });
+
+    it('prototype property 1', function() {
+        fixture('property1', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({
+                type: 'property',
+                name: 'test',
+                string: 'Sample.prototype.test',
+                cons: 'Sample',
+                value: '\'test\''
+            });
+        });
+    });
+
+    it('property 1', function() {
+        fixture('property2', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({
+                type: 'property',
+                name: 'test',
+                string: 'proto.test',
+                receiver: 'proto',
+                value: '\'test\''
+            });
+        });
+    });
+
+    it('declaration 1', function() {
+        fixture('declaration1', function(err, str) {
+            var context = parse(str);
+            expect(context).to.be.a(Object);
+            expect(context).to.eql({
+                type: 'declaration',
+                name: 'TEST',
+                string: 'TEST',
+                value: '123'
+            });
+        });
+    });
 });
