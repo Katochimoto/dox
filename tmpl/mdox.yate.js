21a494ddaafefbb3c3b09b4e33d015ecce50d831
Katochimoto <bufpost@yandex.ru>
2014-02-08 22:02:49 +0400

    update default template

--- tmpl/mdox.yate.js
+++ tmpl/mdox.yate.js
@@ -23,77 +23,60 @@ var yr = yr || require('yate/lib/runtime.js');
 
     var j1 = [ 0, 'type' ];
 
-    function p0(m, c0, i0, l0) {
-        return cmpSN("overview", selectNametest('type', c0, []));
-    }
-
-    var j2 = [ 0, 'tags', 2, p0, 0, 'description' ];
+    var j3 = [ 1, 0 ];
 
     function p1(m, c0, i0, l0) {
-        return cmpSN("overview", selectNametest('type', c0, []));
-    }
-
-    function p2(m, c0, i0, l0) {
-        return cmpSN("fileoverview", selectNametest('type', c0, []));
-    }
-
-    var j3 = [ 0, 'tags', 2, p2, 0, 'description' ];
-
-    function p3(m, c0, i0, l0) {
-        return cmpSN("file", selectNametest('type', c0, []));
-    }
-
-    var j4 = [ 0, 'tags', 2, p3, 0, 'description' ];
-
-    var j6 = [ 1, 0 ];
-
-    function p5(m, c0, i0, l0) {
         return cmpSN("classdesc", selectNametest('type', c0, [])) || cmpSN("description", selectNametest('type', c0, [])) || cmpSN("desc", selectNametest('type', c0, [])) || cmpSN("file", selectNametest('type', c0, [])) || cmpSN("overview", selectNametest('type', c0, [])) || cmpSN("fileoverview", selectNametest('type', c0, []));
     }
 
-    var j7 = [ 0, 'tags', 2, p5 ];
+    var j4 = [ 0, 'tags', 2, p1 ];
 
-    var j8 = [ 0, 'description' ];
+    var j5 = [ 0, 'description' ];
 
-    function p6(m, c0, i0, l0) {
+    function p2(m, c0, i0, l0) {
         return cmpSN("module", selectNametest('type', c0, []));
     }
 
-    var j9 = [ 0, 'tags', 2, p6 ];
+    var j6 = [ 0, 'tags', 2, p2 ];
 
-    var j10 = [ 0, 'name' ];
+    var j7 = [ 0, 'name' ];
 
-    function p7(m, c0, i0, l0) {
+    function p3(m, c0, i0, l0) {
         return cmpSN("version", selectNametest('type', c0, []));
     }
 
-    var j11 = [ 0, 'tags', 2, p7 ];
+    var j8 = [ 0, 'tags', 2, p3 ];
 
-    function p8(m, c0, i0, l0) {
+    function p4(m, c0, i0, l0) {
         return cmpSN("copyright", selectNametest('type', c0, []));
     }
 
-    var j12 = [ 0, 'tags', 2, p8 ];
+    var j9 = [ 0, 'tags', 2, p4 ];
 
-    function p9(m, c0, i0, l0) {
+    function p5(m, c0, i0, l0) {
         return cmpSN("author", selectNametest('type', c0, []));
     }
 
-    var j13 = [ 0, 'tags', 2, p9 ];
+    var j10 = [ 0, 'tags', 2, p5 ];
 
-    // match .* : mdox-file-name
-    M.t0 = function t0(m, c0, i0, l0, a0) {
+    // match .* : mdox-tag-types
+    M.t0 = function t0(m, c0, i0, l0, a0, v0) {
         var r0 = '';
 
+        function p0(m, c0, i0, l0) {
+            return cmpSN(v0, selectNametest('type', c0, []));
+        }
+
+        var j2 = [ 0, 'tags', 2, p0, 0, 'types' ];
+
         r0 += closeAttrs(a0);
-        if (nodeset2boolean( (m.s(j4, c0)) )) {
-            r0 += nodeset2xml( m.s(j4, c0) );
-        } else if (nodeset2boolean( (m.s(j2, c0)) )) {
-            r0 += nodeset2xml( m.s(j2, c0) );
-        } else if (nodeset2boolean( (m.s(j3, c0)) )) {
-            r0 += nodeset2xml( m.s(j3, c0) );
-        } else {
-            r0 += "Undefined";
+        var items0 = (m.s(j2, c0));
+        for (var i1 = 0, l1 = items0.length; i1 < l1; i1++) {
+            var c1 = items0[ i1 ];
+            if ((i1 != 0)) {
+                r0 += "|";
+            }
+            r0 += nodeset2xml( m.s(j3, c1) );
         }
 
         return r0;
@@ -101,33 +84,8 @@ var yr = yr || require('yate/lib/runtime.js');
     M.t0.j = j0;
     M.t0.a = 0;
 
-    // match .* : mdox-tag-types
-    M.t1 = function t1(m, c0, i0, l0, a0, v0) {
-        var r0 = '';
-
-        function p4(m, c0, i0, l0) {
-            return cmpSN(v0, selectNametest('type', c0, []));
-        }
-
-        var j5 = [ 0, 'tags', 2, p4, 0, 'types' ];
-
-        r0 += closeAttrs(a0);
-        var items0 = (m.s(j5, c0));
-        for (var i1 = 0, l1 = items0.length; i1 < l1; i1++) {
-            var c1 = items0[ i1 ];
-            if ((i1 != 0)) {
-                r0 += "|";
-            }
-            r0 += nodeset2xml( m.s(j6, c1) );
-        }
-
-        return r0;
-    };
-    M.t1.j = j0;
-    M.t1.a = 0;
-
     // match .tags[ .type == "classdesc" || .type == "description" || .type == "desc" || .type == "file" || .type == "overview" || .type == "fileoverview" ] : item-tag
-    M.t2 = function t2(m, c0, i0, l0, a0) {
+    M.t1 = function t1(m, c0, i0, l0, a0) {
         var r0 = '';
 
         r0 += closeAttrs(a0);
@@ -135,11 +93,11 @@ var yr = yr || require('yate/lib/runtime.js');
 
         return r0;
     };
-    M.t2.j = j7;
-    M.t2.a = 0;
+    M.t1.j = j4;
+    M.t1.a = 0;
 
     // match .tags[ .type == "module" ] : item-tag
-    M.t3 = function t3(m, c0, i0, l0, a0) {
+    M.t2 = function t2(m, c0, i0, l0, a0) {
         var r0 = '';
 
         r0 += closeAttrs(a0);
@@ -147,11 +105,11 @@ var yr = yr || require('yate/lib/runtime.js');
 
         return r0;
     };
-    M.t3.j = j9;
-    M.t3.a = 0;
+    M.t2.j = j6;
+    M.t2.a = 0;
 
     // match .tags[ .type == "version" ] : item-tag
-    M.t4 = function t4(m, c0, i0, l0, a0) {
+    M.t3 = function t3(m, c0, i0, l0, a0) {
         var r0 = '';
 
         r0 += closeAttrs(a0);
@@ -162,11 +120,11 @@ var yr = yr || require('yate/lib/runtime.js');
 
         return r0;
     };
-    M.t4.j = j11;
-    M.t4.a = 0;
+    M.t3.j = j8;
+    M.t3.a = 0;
 
     // match .tags[ .type == "copyright" ] : item-tag
-    M.t5 = function t5(m, c0, i0, l0, a0) {
+    M.t4 = function t4(m, c0, i0, l0, a0) {
         var r0 = '';
 
         r0 += closeAttrs(a0);
@@ -175,11 +133,11 @@ var yr = yr || require('yate/lib/runtime.js');
 
         return r0;
     };
-    M.t5.j = j12;
-    M.t5.a = 0;
+    M.t4.j = j9;
+    M.t4.a = 0;
 
     // match .tags[ .type == "author" ] : item-tag
-    M.t6 = function t6(m, c0, i0, l0, a0) {
+    M.t5 = function t5(m, c0, i0, l0, a0) {
         var r0 = '';
 
         r0 += closeAttrs(a0);
@@ -188,27 +146,22 @@ var yr = yr || require('yate/lib/runtime.js');
 
         return r0;
     };
-    M.t6.j = j13;
-    M.t6.a = 0;
+    M.t5.j = j10;
+    M.t5.a = 0;
 
     M.matcher = {
-        "mdox-file-name": {
+        "mdox-tag-types": {
             "*": [
                 "t0"
             ]
         },
-        "mdox-tag-types": {
-            "*": [
-                "t1"
-            ]
-        },
         "item-tag": {
             "tags": [
-                "t6",
                 "t5",
                 "t4",
                 "t3",
-                "t2"
+                "t2",
+                "t1"
             ]
         }
     };
